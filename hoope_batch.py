97704efd411fb78fea269204b5ebf7b2cb7a192b
unknown <you@example.com>
2018-11-07 22:30:58 +0900

    feat: add count badge that officials send

--- hoope_batch.py
+++ hoope_batch.py
@@ -8,9 +8,16 @@ import codecs
 import datetime
 import requests
 import dotenv
+import pandas
+import numpy
+from collections import Counter
 from bs4 import BeautifulSoup
+from pandas.tests.io.parser import usecols
+import officials
+from idlelib.iomenu import encoding
 
 dotenv.load_dotenv(verbose=True)
+
 
 def login_to_homelog():
     url = os.environ.get("URL")
@@ -18,7 +25,11 @@ def login_to_homelog():
     user_id = os.environ.get("USER_ID")
     password = os.environ.get("PASSWORD")
 
-    login_payload = {"utf-8": "✓", "admin_admin_user[email]":user_id, "admin_admin_user[password]":password}
+    login_payload = {
+        "utf-8": "✓",
+        "admin_admin_user[email]": user_id,
+        "admin_admin_user[password]": password
+    }
 
     session = requests.Session()
     r = session.get(url)
@@ -29,27 +40,63 @@ def login_to_homelog():
     response = session.post(login_url, data=login_payload)
     return session
 
+
 def get_csv(session):
     datasheet = os.environ.get("DATA_URL")
-    sheet_payload = {"csv[date_from]": "2018/10/01", 'csv[date_to]': "2018/10/31", "csv[badge_id]": ""}
+    sheet_payload = {
+        "csv[date_from]": "2018/10/01",
+        'csv[date_to]': "2018/10/31",
+        "csv[badge_id]": ""
+    }
     response = session.get(datasheet, data=sheet_payload)
     print(response.status_code)
 
     contentType = response.headers['Content-Type']
     contentDisposition = response.headers['Content-Disposition']
     ATTRIBUTE = 'filename='
-    fileName = contentDisposition[contentDisposition.find(ATTRIBUTE) + len(ATTRIBUTE):].replace('\"', "")
+    fileName = contentDisposition[contentDisposition.find(ATTRIBUTE) +
+                                  len(ATTRIBUTE):].replace('\"', "")
 
     saveFileName = datetime.datetime.now().strftime("%Y%m%d") + fileName
-#     print(saveFileName)
+    #     print(saveFileName)
     saveFilePath = "C:\Program Files (x86)\pleiades\workspace\hoope_batch\Data\CP932\%s" % saveFileName
-#     print(saveFilePath)
+    #     print(saveFilePath)
     with open(saveFilePath, 'wb') as saveFile:
         saveFile.write(response.content)
     return saveFilePath
+
+
+def extract_sender(source_file):
+    with open(source_file, "r", encoding="CP932") as fin:
+        reader = pandas.read_csv(fin, usecols=[1])
+        #         print(type(reader.values))
+        processed_list = numpy.ravel(reader.values)
+        counter = Counter(processed_list)
+        num_list = {}
+        for word, cnt in counter.most_common():
+            num_list[word] = cnt
+
+
+#         print(num_list)
+        return num_list
+
+
+def count_officials_sending(sending_dict):
+    official_list = officials.namelist
+    #print(official_list)
+    officials_send_counter = {}
+    for person in official_list:
+        try:
+            officials_send_counter[person] = sending_dict[person]
+        except:
+            officials_send_counter[person] = 0
+    print(officials_send_counter)
+
 
 if __name__ == '__main__':
     print("start")
     session = login_to_homelog()
     source_file = get_csv(session)
+    sending_dict = extract_sender(source_file)
+    count_officials_sending(sending_dict)
     print("end")
