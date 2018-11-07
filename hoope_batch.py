5d10cf965456d5fbb89a866f44ee8a4a939e1981
unknown <you@example.com>
2018-11-07 21:09:58 +0900

    feat: add csv_getter

--- hoope_batch.py
+++ hoope_batch.py
@@ -4,6 +4,8 @@ Created on 2018/10/27
 @author: Administrator
 '''
 import os
+import codecs
+import datetime
 import requests
 import dotenv
 from bs4 import BeautifulSoup
@@ -13,7 +15,6 @@ dotenv.load_dotenv(verbose=True)
 def login_to_homelog():
     url = os.environ.get("URL")
     login_url = os.environ.get("LOGIN_URL")
-    datasheet = os.environ.get("DATA_URL")
     user_id = os.environ.get("USER_ID")
     password = os.environ.get("PASSWORD")
 
@@ -25,12 +26,30 @@ def login_to_homelog():
     auth_token = soup.find(attrs={'name': 'authenticity_token'}).get('value')
     login_payload['authenticity_token'] = auth_token
 
-    session.post(login_url, data=login_payload)
-    sheet_payload = {"utf-8": "✓", 'authenticity_token': auth_token, "month": "2018/09", "type": "1", "badgetype": "1"}
-    response = session.post(datasheet, data=sheet_payload)
-    print(response.content)
+    response = session.post(login_url, data=login_payload)
+    return session
+
+def get_csv(session):
+    datasheet = os.environ.get("DATA_URL")
+    sheet_payload = {"csv[date_from]": "2018/10/01", 'csv[date_to]': "2018/10/31", "csv[badge_id]": ""}
+    response = session.get(datasheet, data=sheet_payload)
+    print(response.status_code)
+
+    contentType = response.headers['Content-Type']
+    contentDisposition = response.headers['Content-Disposition']
+    ATTRIBUTE = 'filename='
+    fileName = contentDisposition[contentDisposition.find(ATTRIBUTE) + len(ATTRIBUTE):].replace('\"', "")
+
+    saveFileName = datetime.datetime.now().strftime("%Y%m%d") + fileName
+#     print(saveFileName)
+    saveFilePath = "C:\Program Files (x86)\pleiades\workspace\hoope_batch\Data\CP932\%s" % saveFileName
+#     print(saveFilePath)
+    with open(saveFilePath, 'wb') as saveFile:
+        saveFile.write(response.content)
+    return saveFilePath
 
 if __name__ == '__main__':
     print("start")
-    login_to_homelog()
+    session = login_to_homelog()
+    source_file = get_csv(session)
     print("end")
