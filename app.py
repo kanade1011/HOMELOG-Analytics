10f1ef6c96205defa11dadf5dcac54c2dfa11096
unknown <you@example.com>
2018-11-15 22:20:49 +0900

    add sample template

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 import services.batch_service as service
 
-# app = Flask(__name__)
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    title = "index"
+    str = "foo"
+    return render_template('index.html', str=str, title=title)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    title = "こんにちは"
+    if request.method == 'POST':
+        # リクエストフォームから「名前」を取得して
+        name = request.form['name']
+        # index.html をレンダリングする
+        return render_template('index.html',
+                               name=name, title=title)
+    else:
+        # エラーなどでリダイレクトしたい場合はこんな感じで
+        return redirect(url_for('index'))
 
 
 def create_collection():
@@ -19,3 +40,6 @@ collection.insert_one(post)
 
 record = collection.find_one({"month": "10"})
 print(record['body'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
