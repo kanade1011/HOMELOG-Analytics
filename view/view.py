e7ab9ec52f63578831663a6c2e011fa7e94b3fef
unknown <you@example.com>
2018-11-23 12:00:23 +0900

    fix bug

--- view/view.py
+++ view/view.py
@@ -8,15 +8,15 @@ today = datetime.date.today()
 
 @view.route('/<specified_month>')
 def view_month_summry(specified_month=None):
-    month = specified_month or '%s' % (today.month-1)
+    month = specified_month or '%s' % (today.month - 1)
     result = result_getter.result_getter(month=month)
     print(result)
-    return render_template('month_summry.html', result=result, month=month)\
+    return render_template('month_summry.html', result=result, month=month)
 
 
 @view.route('/')
 def view_this_month_summry():
-    month = '%s' % (today.month-1)
+    month = '%s' % (today.month - 1)
     result = result_getter.result_getter(month=month)
     print(result)
     return render_template('month_summry.html', result=result, month=month)
